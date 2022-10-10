cfcb29ff7907383067d5b8c3a93b9f006fba7136
Mollis-Kim <himuchik09@gmail.com>
2022-10-10 19:51:30 +0900

    문풀

--- 29471.cpp
+++ 29471.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string alph[8] = {"c=", "c-", "dz=", "d-", "lj", "nj","s=","z="};
+string cro[8] = {"c=", "c-", "dz=", "d-", "lj", "nj","s=","z="};
 
 int main(){
     ios::sync_with_stdio(false);
     string input;
     cin>>input;
-    int cnt=0;
+    
     for(int i=0; i<8; i++){
-        int pos=0;
-        while(pos!=string::npos){
-            pos = input.find(alph[i],pos);
-            if(pos!=string::npos){
-                cnt++;
-                pos++;
+        size_t pos = 0;
+        while((pos=input.find(cro[i],pos)) - string::npos !=0){
+            if(i == 2){
+                input.replace(pos,3," ");
+            }else{
+                input.replace(pos,2," ");
             }
         }
     }
-    cout<<cnt;
+    cout<< input.size();
     return 0;
 }
